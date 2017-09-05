4a5f76f680b75fcabfb6b6c2ed4de4025fcbfdab
Meghan Spencer <emjayess@gmail.com>
2017-09-05 21:11:01 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -1,8 +1,9 @@
-const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
+const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65];
+
 
 function init() {
   // Write your JavaScript code inside the init() function
-  let body = document.getElementsByTagName('body');
+  let body = document.querySelector('body');
   let index = 0;
 
   body.addEventListener('keydown', function(e) {
